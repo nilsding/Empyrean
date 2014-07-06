f83242901e9dfe18f02f01447bf23b211e88c238
nilsding <nilsding@nilsding.org>
2014-07-06 20:20:54 +0200

    added hashtags to the HTML output

--- lib/templaterenderer.rb
+++ lib/templaterenderer.rb
@@ -35,6 +35,7 @@ class TemplateRenderer
   # Renders @template.
   def render
     mentions = mentions_erb
+    hashtags = hashtags_erb
     clients = clients_erb
     erb = ERB.new @template
     erb.result binding
@@ -50,13 +51,38 @@ class TemplateRenderer
       }
       top10 = @parsed[:mentions].slice(0, 10) # top 10 mentions
       top10.each do |mention|
-        retdict[:top10] << { name: mention[0] }.merge(mention[1])
+        retdict[:top10] << mention[1]
       end
       
       nottop = @parsed[:mentions].slice(11, 20) # not in the top 10
-      nottop.each do |mention|
-        mention[1].delete(:example)
-        retdict[:nottop] << { name: mention[0] }.merge(mention[1])
+      unless nottop.nil?
+        nottop.each do |mention|
+          mention[1].delete(:example)
+          retdict[:nottop] << mention[1]
+        end
+      end
+      
+      retdict
+    end
+    
+    ##
+    # Returns an array with the hashtags which can be easily used within ERB.
+    def hashtags_erb
+      retdict = {
+        top10: [],
+        nottop: []
+      }
+      top10 = @parsed[:hashtags].slice(0, 10) # top 10 mentions
+      top10.each do |hashtag|
+        retdict[:top10] << hashtag[1]
+      end
+      
+      nottop = @parsed[:hashtags].slice(11, 20) # not in the top 10
+      unless nottop.nil?
+        nottop.each do |hashtag|
+          hashtag[1].delete(:example)
+          retdict[:nottop] << hashtag[1]
+        end
       end
       
       retdict
@@ -80,14 +106,16 @@ class TemplateRenderer
       end
       
       nottop = @parsed[:clients].slice(11, 20) # not in the top 10
-      nottop.each do |client|
-        client[1].delete(:example)
-        retdict[:nottop] << {
-          name: client[1][:name],
-          url: client[1][:url],
-          count: client[1][:count],
-          percentage: (client[1][:count] * 100 / @parsed[:tweet_count].to_f).round(2)
-        }
+      unless nottop.nil?
+        nottop.each do |client|
+          client[1].delete(:example)
+          retdict[:nottop] << {
+            name: client[1][:name],
+            url: client[1][:url],
+            count: client[1][:count],
+            percentage: (client[1][:count] * 100 / @parsed[:tweet_count].to_f).round(2)
+          }
+        end
       end
       
       retdict
